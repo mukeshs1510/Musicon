81a1c72dc24b36f25042a75d2edf78bb14496fb5
Mukesh Suthar <49620706+mukeshs1510@users.noreply.github.com>
2021-03-17 15:25:07 +0530

    SecondCommit: After adding the forground services to the app

---
+++ app/src/main/java/com/mbs/musicon/ExoPlayer/FirebaseMusicSource.kt
@@ -0,0 +1,39 @@
+package com.mbs.musicon.ExoPlayer
+
+class FirebaseMusicSource {
+
+    private val onReadyListener = mutableListOf<(Boolean) -> Unit>()
+
+    private var state = State.STATE_CREATED
+        set(value) {
+            if(value == State.STATE_INITIALIZED || value == State.STATE_ERROR) {
+                synchronized(onReadyListener) {
+                    field = value
+                    onReadyListener.forEach {
+                        listner -> listner(state == State.STATE_INITIALIZED)
+                    }
+                }
+            } else {
+                field = value
+            }
+        }
+
+    fun whenReady(action: (Boolean) -> Unit): Boolean {
+        if(state == State.STATE_CREATED || state == State.STATE_INITIALIZING) {
+            onReadyListener += action
+            return false
+        } else {
+            action(state == State.STATE_INITIALIZED)
+            return true
+        }
+    }
+
+}
+
+enum class State{
+    STATE_CREATED,
+    STATE_INITIALIZING,
+    STATE_INITIALIZED,
+    STATE_ERROR,
+}
+
